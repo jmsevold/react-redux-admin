aecf74060954d79b8949a0ab6bb3c4c8b4d342e0
jmsevold <jmsevold@gmail.com>
2016-08-03 17:32:18 +0000

    router added to entry point

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import 'babel-polyfill';
+import { Router, browserHistory } from 'react-router';
+import routes from './routes';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+//import App from './App';
+//import './index.css';
+import './styles/styles.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
-  <App />,
+  <Router history={browserHistory} routes={routes} />,
   document.getElementById('root')
 );
