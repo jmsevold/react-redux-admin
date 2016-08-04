53e45dcbd33758b203415c0f9f37726645731bf8
jmsevold <jmsevold@gmail.com>
2016-08-04 20:59:55 +0000

    added root reducer

--- src/reducers/courseReducer.js
+++ src/reducers/courseReducer.js
@@ -1,9 +1,12 @@
 export default function courseReducer(state = [], action) {
   switch(action.type){
     case 'CREATE_COURSE':
-     return [
-       ...state,
-       Object.assign({}, action.course)
+      return [
+        ...state,
+        Object.assign({}, action.course)
      ];
+     
+    default:
+      return state;
   }
 }
