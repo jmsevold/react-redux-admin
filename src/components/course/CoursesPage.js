35cf4d5c3ec3e60467a73b20457299e6bc9414b1
jmsevold <jmsevold@gmail.com>
2016-08-04 18:22:19 +0000

    add course in the CoursesPage

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -3,10 +3,44 @@ import React, { Component, PropTypes } from 'react';
 
 
 class CoursesPage extends Component {
+  constructor(props,context){
+    super(props,context);
+    this.state = {
+      course: {title: ''}  
+    };
+    this.onTitleChange = this.onTitleChange.bind(this);
+    this.onSaveCourse = this.onSaveCourse.bind(this);
+  }
+  
+  onTitleChange(e){
+    const course = this.state.course;
+    course.title = e.target.value;
+    this.setState({
+      course: course
+    });
+    console.log(`${this.state.course.title}`);
+  }
+  
+  onSaveCourse(){
+    console.log(`saving course ${this.state.course.title}`);   
+  }
+  
   render() {
     return (
       <div>
         <h1>Courses</h1>
+        <h2>Add Course</h2>
+        <input
+          type="text"
+          onChange={this.onTitleChange}
+          value={this.state.course.title}
+        />
+        
+        <input
+          type="submit"
+          onClick={this.onSaveCourse}
+          value="Save"
+        />
       </div>
     );
   }
